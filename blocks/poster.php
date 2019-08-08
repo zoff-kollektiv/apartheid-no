bfdf2f7dfeb9bd14ffef8a440adca63186f9f26e
Gustav Pursche <pursche@posteo.de>
2019-08-08 21:35:18 +0200

    Add poster block

---
+++ blocks/poster.php
@@ -0,0 +1,42 @@
+<div class="constraint">
+  <div class="poster">
+    <?php
+      $images_count = sizeof(get_field('images'));
+
+      while(have_rows('images')):
+        the_row();
+
+        $poster_id = get_sub_field('poster');
+        $poster = get_post($poster_id);
+    ?>
+
+      <figure class="figure figure--is-<?php if ($images_count == 1):  echo 'reversed'; else: echo 'columnized'; endif; ?>">
+          <?php echo wp_get_attachment_image($poster_id, 'full'); ?>
+
+          <figcaption class="figure__caption-container">
+            <?php if($poster->post_title) : ?>
+              <strong class="figure__title">
+                <?php echo $poster->post_title; ?>
+              </strong>
+            <?php endif; ?>
+
+            <?php if($poster->post_content) : ?>
+              <p class="figure__description">
+                <?php echo $poster->post_content; ?>
+              </p>
+            <?php endif; ?>
+
+            <?php if($poster->post_excerpt) : ?>
+              <small class="figure__caption">
+                <?php echo $poster->post_excerpt; ?>
+              </small>
+            <?php endif; ?>
+          </figcaption>
+      </figure>
+
+    <?php
+      endwhile;
+    ?>
+
+  </div>
+</div>
