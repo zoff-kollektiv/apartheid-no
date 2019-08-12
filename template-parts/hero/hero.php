b8db32dc98c629c606c35a18ec8c91f2071b04d5
Gustav Pursche <pursche@posteo.de>
2019-08-12 13:36:17 +0200

    Hero: render featured image

--- template-parts/hero/hero.php
+++ template-parts/hero/hero.php
@@ -1,4 +1,6 @@
 <section class="hero">
+  <?php echo get_the_post_thumbnail(null, 'post-thumbnail', [ 'class' => 'hero__image' ]); ?>
+
   <div class="constraint">
     <h1 class="hero__title">
       <small class="hero__chapter">Kapitel 1</small>
