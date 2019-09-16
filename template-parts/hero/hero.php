2a88438f275a63a8e41ad77557f0ad0719826895
Gustav Pursche <pursche@posteo.de>
2019-09-16 18:04:42 +0200

    Hero: center title on front page

--- template-parts/hero/hero.php
+++ template-parts/hero/hero.php
@@ -1,12 +1,28 @@
-<section class="hero">
+<?php
+$modifier = '';
+
+if(is_front_page()) {
+  $modifier = 'hero--is-centered';
+}
+
+?>
+
+<section class="hero <?php echo $modifier; ?>">
   <?php echo get_the_post_thumbnail(null, 'post-thumbnail', [
       'class' => 'hero__image'
   ]); ?>
 
   <div class="constraint">
     <h1 class="hero__title">
-      <small class="hero__chapter">Kapitel 1</small>
-      <?php the_title(); ?>
+      <?php if(is_front_page()) : ?>
+        <?php echo get_bloginfo('name'); ?>
+        <p class="hero__chapter-subline">
+          <?php echo get_bloginfo('description'); ?>
+        </p>
+      <?php else : ?>
+        <small class="hero__chapter">Kapitel 1</small>
+        <?php the_title(); ?>
+      <?php endif; ?>
     </h1>
   </div>
 </section>
