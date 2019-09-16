6713e80dbbd750e6a4c2d70cf877df9a91c5b864
Gustav Pursche <pursche@posteo.de>
2019-09-16 17:54:47 +0200

    Excerpt: fix color on full-bleed utilities

--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -1,6 +1,6 @@
 <div class="full-bleed full-bleed--fill-blue">
   <div class="constraint">
-    <em class="excerpt">
+    <em class="excerpt excerpt--is-inverted">
       <?php echo get_the_excerpt(); ?>
     </em>
   </div>
