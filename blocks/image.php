2849605e46abffbc81656b4336593d08f1a63e7f
Gustav Pursche <pursche@posteo.de>
2019-09-17 19:33:13 +0200

    Media/ Image/ Poster: enhance vertical alignment

--- blocks/image.php
+++ blocks/image.php
@@ -1,30 +1,32 @@
 <?php
 $image = get_field('image'); ?>
 
-<div class="constraint">
-  <figure class="figure">
-    <?php echo wp_get_attachment_image($image['ID'], 'image', null, [
-        'class' => 'figure__media'
-    ]); ?>
+<div class="full-bleed full-bleed--with-margin">
+  <div class="constraint">
+    <figure class="figure figure--is-columnized">
+      <?php echo wp_get_attachment_image($image['ID'], 'image', null, [
+          'class' => 'figure__media'
+      ]); ?>
 
-    <figcaption class="figure__caption-container">
-      <?php if ($image['title']): ?>
-        <strong class="figure__title">
-          <?php echo $image['title']; ?>
-        </strong>
-      <?php endif; ?>
+      <figcaption class="figure__caption-container">
+        <?php if ($image['title']): ?>
+          <strong class="figure__title">
+            <?php echo $image['title']; ?>
+          </strong>
+        <?php endif; ?>
 
-      <?php if ($image['description']): ?>
-        <p class="figure__description">
-          <?php echo $image['description']; ?>
-        </p>
-      <?php endif; ?>
+        <?php if ($image['description']): ?>
+          <p class="figure__description">
+            <?php echo $image['description']; ?>
+          </p>
+        <?php endif; ?>
 
-      <?php if ($image['caption']): ?>
-        <small class="figure__caption">
-          <?php echo $image['caption']; ?>
-        </small>
-      <?php endif; ?>
-    </figcaption>
-  </figure>
+        <?php if ($image['caption']): ?>
+          <small class="figure__caption">
+            <?php echo $image['caption']; ?>
+          </small>
+        <?php endif; ?>
+      </figcaption>
+    </figure>
+  </div>
 </div>
--- blocks/poster.php
+++ blocks/poster.php
@@ -1,49 +1,51 @@
-<div class="constraint">
-  <div class="poster">
-    <?php
-    $images_count = sizeof(get_field('images'));
+<div class="full-bleed full-bleed--with-margin">
+  <div class="constraint">
+    <div class="poster">
+      <?php
+      $images_count = sizeof(get_field('images'));
 
-    while (have_rows('images')):
+      while (have_rows('images')):
 
-        the_row();
+          the_row();
 
-        $poster_id = get_sub_field('poster');
-        $poster = get_post($poster_id);
-        ?>
+          $poster_id = get_sub_field('poster');
+          $poster = get_post($poster_id);
+          ?>
 
-      <figure class="figure figure--is-<?php if ($images_count == 1):
-          echo 'reversed';
-      else:
-          echo 'columnized';
-      endif; ?>">
-        <?php echo wp_get_attachment_image($poster_id, 'poster', null, [
-            'class' => 'figure__media'
-        ]); ?>
+        <figure class="figure figure--is-<?php if ($images_count == 1):
+            echo 'reversed';
+        else:
+            echo 'columnized';
+        endif; ?>">
+          <?php echo wp_get_attachment_image($poster_id, 'poster', null, [
+              'class' => 'figure__media'
+          ]); ?>
 
-        <figcaption class="figure__caption-container">
-          <?php if ($poster->post_title): ?>
-            <strong class="figure__title">
-              <?php echo $poster->post_title; ?>
-            </strong>
-          <?php endif; ?>
+          <figcaption class="figure__caption-container">
+            <?php if ($poster->post_title): ?>
+              <strong class="figure__title">
+                <?php echo $poster->post_title; ?>
+              </strong>
+            <?php endif; ?>
 
-          <?php if ($poster->post_content): ?>
-            <p class="figure__description">
-              <?php echo $poster->post_content; ?>
-            </p>
-          <?php endif; ?>
+            <?php if ($poster->post_content): ?>
+              <p class="figure__description">
+                <?php echo $poster->post_content; ?>
+              </p>
+            <?php endif; ?>
 
-          <?php if ($poster->post_excerpt): ?>
-            <small class="figure__caption">
-              <?php echo $poster->post_excerpt; ?>
-            </small>
-          <?php endif; ?>
-        </figcaption>
-      </figure>
+            <?php if ($poster->post_excerpt): ?>
+              <small class="figure__caption">
+                <?php echo $poster->post_excerpt; ?>
+              </small>
+            <?php endif; ?>
+          </figcaption>
+        </figure>
 
-    <?php
-    endwhile;
-    ?>
+      <?php
+      endwhile;
+      ?>
 
+    </div>
   </div>
 </div>
