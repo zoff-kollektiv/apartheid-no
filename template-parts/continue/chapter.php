00833f2d846b2c40fb2599facfe81ed96acbcdc4
Gustav Pursche <pursche@posteo.de>
2019-09-17 13:31:48 +0200

    Next: add summary

--- template-parts/continue/chapter.php
+++ template-parts/continue/chapter.php
@@ -1,9 +1,15 @@
 <?php
-$next = get_next_post(); ?>
+$next = get_next_post();
+$summary = get_field('summary');
+?>
 
 <?php if ($next): ?>
   <section class="next full-bleed full-bleed--fill-red">
     <div class="constraint">
+      <?php if($summary): ?>
+        <em class="excerpt excerpt--is-inverted"><?php echo $summary; ?></em>
+      <?php endif; ?>
+
       <a href="<?php echo get_the_permalink($next->ID); ?>" class="next__link">
       <small class="next__link-label">Nächstes Kapitel lesen:</small>
       <?php echo $next->post_title; ?>
