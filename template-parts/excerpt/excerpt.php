474ee43f65bd5bb5be3f328bf2c41fe75b254213
Gustav Pursche <pursche@posteo.de>
2019-09-17 16:29:21 +0200

    Navigation: remove details arrow on for webkit browsers

--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -1,4 +1,4 @@
-<div class="full-bleed full-bleed--fill-blue">
+<div class="full-bleed full-bleed--fill-blue full-bleed--with-margin-bottom">
   <div class="constraint">
     <em class="excerpt excerpt--is-inverted">
       <?php echo get_the_excerpt(); ?>
