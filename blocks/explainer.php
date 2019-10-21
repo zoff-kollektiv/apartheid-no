317273e06c6181e24e0f8458f79d560081616f1d
Gustav Pursche <pursche@posteo.de>
2019-10-21 11:58:37 +0200

    Explainer: enhance alignment

--- blocks/explainer.php
+++ blocks/explainer.php
@@ -3,56 +3,57 @@ $term = get_field('term');
 $intro = get_field('intro');
 ?>
 
-
-<details class="explainer">
-  <summary class="explainer__summary">
-    <div class="constraint">
-      <em class="explainer__term"><?php echo $term; ?>,</em>
-      <p class="explainer__intro"><?php echo $intro; ?></p>
-    </div>
-  </summary>
-
-  <?php while (have_rows('explanation')):
-      the_row(); ?>
-    <?php if (get_row_layout() == 'text'): ?>
+<div class="constraint">
+  <details class="explainer">
+    <summary class="explainer__summary">
       <div class="constraint">
-        <div class="paragraph">
-          <?php echo get_sub_field('text'); ?>
+        <em class="explainer__term"><?php echo $term; ?>,</em>
+        <p class="explainer__intro"><?php echo $intro; ?></p>
+      </div>
+    </summary>
+
+    <?php while (have_rows('explanation')):
+        the_row(); ?>
+      <?php if (get_row_layout() == 'text'): ?>
+        <div class="constraint">
+          <div class="paragraph">
+            <?php echo get_sub_field('text'); ?>
+          </div>
         </div>
-      </div>
-    <?php endif; ?>
+      <?php endif; ?>
 
-    <?php if (get_row_layout() == 'image'):
-        $image = get_sub_field('image'); ?>
-      <div class="constraint">
-        <figure class="figure">
-          <?php echo wp_get_attachment_image($image['ID'], 'image', null, [
-              'class' => 'figure__media'
-          ]); ?>
+      <?php if (get_row_layout() == 'image'):
+          $image = get_sub_field('image'); ?>
+        <div class="constraint">
+          <figure class="figure">
+            <?php echo wp_get_attachment_image($image['ID'], 'image', null, [
+                'class' => 'figure__media'
+            ]); ?>
 
-          <figcaption class="figure__caption-container">
-            <?php if ($image['title']): ?>
-              <strong class="figure__title">
-                <?php echo $image['title']; ?>
-              </strong>
-            <?php endif; ?>
+            <figcaption class="figure__caption-container">
+              <?php if ($image['title']): ?>
+                <strong class="figure__title">
+                  <?php echo $image['title']; ?>
+                </strong>
+              <?php endif; ?>
 
-            <?php if ($image['description']): ?>
-              <p class="figure__description">
-                <?php echo $image['description']; ?>
-              </p>
-            <?php endif; ?>
+              <?php if ($image['description']): ?>
+                <p class="figure__description">
+                  <?php echo $image['description']; ?>
+                </p>
+              <?php endif; ?>
 
-            <?php if ($image['caption']): ?>
-              <small class="figure__caption">
-                <?php echo $image['caption']; ?>
-              </small>
-            <?php endif; ?>
-          </figcaption>
-        </figure>
-      </div>
+              <?php if ($image['caption']): ?>
+                <small class="figure__caption">
+                  <?php echo $image['caption']; ?>
+                </small>
+              <?php endif; ?>
+            </figcaption>
+          </figure>
+        </div>
+      <?php
+      endif; ?>
     <?php
-    endif; ?>
-  <?php
-  endwhile; ?>
-</details>
+    endwhile; ?>
+  </details>
+</div>
