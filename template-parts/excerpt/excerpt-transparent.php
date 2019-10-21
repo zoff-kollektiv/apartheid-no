063482d29de22cd60800180c41ad0a436f5cd57b
Gustav Pursche <pursche@posteo.de>
2019-10-21 16:30:55 +0200

    Excerpt: fix background color of transparent version

--- template-parts/excerpt/excerpt-transparent.php
+++ template-parts/excerpt/excerpt-transparent.php
@@ -1,5 +1,5 @@
 <?php if (has_excerpt()): ?>
-  <div class="full-bleed full-bleed--fill-blue full-bleed--with-margin-bottom">
+  <div class="full-bleed full-bleed--with-margin-bottom">
     <div class="constraint">
       <em class="excerpt">
         <?php the_excerpt(); ?>
