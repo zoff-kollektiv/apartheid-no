4a0ff72eab75672857ab25a9e11ce3cac2d45cf5
Gustav Pursche <pursche@posteo.de>
2019-10-21 12:03:16 +0200

    Excerpt: display featured image caption

--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -3,5 +3,9 @@
     <em class="excerpt excerpt--is-inverted">
       <?php echo get_the_excerpt(); ?>
     </em>
+
+    <small class="excerpt__featured-image-caption">
+      <?php the_post_thumbnail_caption(); ?>
+    </small>
   </div>
 </div>
