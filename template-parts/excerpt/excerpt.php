5d56ed2d6350a9c9ae7aee99fc7f89f2225284e1
Gustav Pursche <pursche@posteo.de>
2019-10-21 13:45:42 +0200

    Excerpt: render caption only if one is set

--- template-parts/excerpt/excerpt.php
+++ template-parts/excerpt/excerpt.php
@@ -1,11 +1,16 @@
+<?php
+$caption = get_the_post_thumbnail_caption(); ?>
+
 <div class="full-bleed full-bleed--fill-blue full-bleed--with-margin-bottom">
   <div class="constraint">
     <em class="excerpt excerpt--is-inverted">
       <?php echo get_the_excerpt(); ?>
     </em>
 
-    <small class="excerpt__featured-image-caption">
-      <?php the_post_thumbnail_caption(); ?>
-    </small>
+    <?php if ($caption): ?>
+      <small class="excerpt__featured-image-caption">
+        <?php echo $caption; ?>
+      </small>
+    <?php endif; ?>
   </div>
 </div>
